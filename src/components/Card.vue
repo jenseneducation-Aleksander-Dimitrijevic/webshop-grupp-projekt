6b33f486e232a280417ea1f4e301ce9b79198ce9
maxaakre <maxaakre@gmail.com>
2020-08-31 21:35:09 +0200

    Done style for card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="wrapper">
+  <div class="card">
     <div>
+      <h1 class="vans">Vans Old school</h1>
       <img src="../assets/vans1.png" alt="vans" />
-      <p
-        class="info"
-      >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati totam corporis similique eligendi doloremque exercitationem odit praesentium quos qui reiciendis, delectus ipsam saepe in ratione voluptatibus. Illum numquam maxime voluptas.</p>
       <p class="price">
         <span>800kr</span>
+      </p>
+      <p class="info">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
+        totam corporis similique eligendi doloremque exercitationem odit
+        praesentium quos qui reiciendis, delectus ipsam saepe in ratione
+        voluptatibus. Illum numquam maxime voluptas.
       </p>
       <button class="cart">Add to basket</button>
     </div>
@@ -18,15 +22,19 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.wrapper {
+.card {
   display: flex;
   max-width: 375px;
-  padding: 0 1rem;
-  h1 {
+  padding: 1rem;
+  .vans {
     color: #b56161;
+    text-align: left;
+    padding: 1rem 0;
   }
-  p {
+  .info {
     max-width: 300px;
+    padding: 2rem 0;
+    font-size: 18px;
   }
   img {
     width: 100%;
@@ -35,12 +43,14 @@ export default {};
   }
   .price {
     text-align: left;
+    font-size: 24px;
+    padding: 2rem 0 0 0;
   }
   button {
     background: #b56161;
-    width: 300px;
+    width: 100%;
     padding: 1rem;
-    margin: auto;
+    margin: 1rem 0;
     border-radius: 5px;
     font-weight: bold;
     font-size: 24px;
