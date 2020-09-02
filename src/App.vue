18f72d8db44610c4a9a23e29c44bbb0a220bdf8e
Aleksander Dimitrijevic <aleksander88.dev@gmail.com>
2020-09-02 17:06:49 +0200

    changed global font

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,14 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap");
 * {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
+}
+
+body {
+  font-family: "Open Sans", sans-serif;
 }
 </style>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,10 +1,60 @@
 <template>
-  <div class="name">{{$route.params.name}}</div>
+  <div class="container">
+    <h1>Shoes</h1>
+    <ul class="product-list">
+      <li class="list-item" v-for="product in products" :key="product.id">
+        <span class="product-name">{{ product.name }}</span>
+        <img :src="require('../assets/'+product.img+'.png')" alt="product-image" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  async created() {
+    const RESPONSE = await axios.get("http://localhost:3000/products");
+    this.products = RESPONSE.data;
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.container {
+  padding: 1rem;
+  margin: 50px 0;
+
+  h1 {
+    margin-bottom: 2rem;
+  }
+
+  .product-list {
+    display: grid;
+    list-style: none;
+    grid-template-columns: 1fr;
+
+    .list-item {
+      width: 100%;
+      height: 300px;
+      display: block;
+      border-radius: 3px;
+      background: no-repeat center center/100%;
+
+      .product-name {
+        font-size: 1.3rem;
+      }
+
+      img {
+        width: 100%;
+        display: block;
+      }
+    }
+  }
+}
 </style>
