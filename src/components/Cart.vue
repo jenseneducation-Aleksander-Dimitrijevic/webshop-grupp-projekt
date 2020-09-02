f32576a485e2ddbdba79daaf1c91ed7c6e1d6016
Patrick Carragher <carragher@live.se>
2020-09-02 21:58:11 +0200

    initial commit,  Some Tests Done, failing within parameters

---
+++ src/components/Cart.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="wrapper">
+		<span>{{count}}</span>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				count: 1
+			}
+		},
+		methods:{
+			increment(){
+				this.count = 2
+			},
+			decrement(){
+				this.count = 1
+			}
+
+
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
