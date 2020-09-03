f33fe38e6429a2aefc4374a0ab0379d0bbe7b21b
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2020-09-03 14:55:57 +0200

    stash

--- tests/unit/Navbar.spec.js
+++ tests/unit/Navbar.spec.js
@@ -1,27 +1,23 @@
-import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
-import Navbar from '@/components/Navbar.vue';
-import VueRouter from 'vue-router'
-import Products from '@/views/Products.vue';
-import routes from '@/router/index.js';
+import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
+import Navbar from "@/components/Navbar.vue";
+import VueRouter from "vue-router";
+import Products from "@/views/Products.vue";
+import routes from "@/router/index.js";
 
-const localVue = createLocalVue()
-localVue.use(VueRouter)
+const localVue = createLocalVue();
+localVue.use(VueRouter);
 
-describe('Navbar', () => {
+describe("Navbar", () => {
+  test("render component via routing", async () => {
+    const router = new VueRouter({ routes, mode: "history" });
+    const wrapper = mount(Navbar, {
+      localVue,
+      router,
+    });
 
-    test('render component via routing', async () => {
+    router.push("/products");
+    await wrapper.vm.$nextTick();
 
-        const router = new VueRouter({ routes, mode: 'history' })
-        const wrapper = mount(Navbar, {
-            localVue,
-            router
-        })
-
-        router.push('/products')
-        await wrapper.vm.$nextTick()
-
-        expect(wrapper.findComponent(Products).exists()).toBe(true)
-
-    })
-
-})
+    expect(wrapper.findComponent(Products).exists()).toBe(true);
+  });
+});
