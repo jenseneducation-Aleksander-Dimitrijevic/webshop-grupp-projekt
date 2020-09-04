99a31c8da2036a0d15f15c5329764782e515e730
Aleksander Dimitrijevic <aleksander.dimitrijevic@gmail.com>
2020-09-05 00:31:49 +0200

    attached page transition on each route navigation

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <Navbar />
     <app-side-menu />
-    <router-view />
+    <transition name="slide-fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -26,6 +28,23 @@ export default {
 }
 
 body {
+  overflow-x: hidden;
   font-family: "Open Sans", sans-serif;
+}
+
+// Page transition
+.slide-fade-enter {
+  transform: translateX(10px);
+  opacity: 0;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 0.2s ease;
+}
+
+.slide-fade-leave-to {
+  transform: translateX(-10px);
+  opacity: 0;
 }
 </style>
