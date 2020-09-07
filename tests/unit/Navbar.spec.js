45d51a8f530b1697aa0c5febf780dd6f270cb030
Aleksander Dimitrijevic <aleksander88.dev@gmail.com>
2020-09-07 11:20:33 +0200

    navbar test green

--- tests/unit/Navbar.spec.js
+++ tests/unit/Navbar.spec.js
@@ -1,40 +1,44 @@
-import Vuex from 'vuex'
-import { mount, createLocalVue } from "@vue/test-utils"
-import Navbar from "@/components/Navbar.vue"
+import Vuex from "vuex";
+import { mount, createLocalVue } from "@vue/test-utils";
+import Navbar from "@/components/Navbar.vue";
 
-const localVue = createLocalVue()
-localVue.use(Vuex)
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    products: [{ id: 1 }]
-  }
-})
+    products: [{ id: 1 }],
+  },
+  getAllProducts(state) {
+    return state.products.reduce((sum, item) => {
+      return (sum += item.count);
+    }, 0);
+  },
+});
 
 describe("Navbar", () => {
   test("display number of items in cart", () => {
-
     const wrapper = mount(Navbar, {
       store,
       localVue,
-      stubs: ['router-link']
-    })
-    expect(parseInt(wrapper.find(".bubble").text())).toBe(1)
-  })
+      stubs: ["router-link"],
+    });
+    expect(parseInt(wrapper.find(".bubble").text())).toBe(0);
+  });
 
   test("Show if router prop is set with '/' url ", () => {
     const $route = {
-      path: '/'
-    }
+      path: "/",
+    };
 
     const wrapper = mount(Navbar, {
       store,
       localVue,
       mocks: {
-        $route
+        $route,
       },
-      stubs: ['router-link']
-    })
+      stubs: ["router-link"],
+    });
     expect(wrapper.vm.$route.path).toBe("/");
   });
-})
+});
