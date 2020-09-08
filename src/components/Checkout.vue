221a5237c0dc10a1f905d825f252e5c4fc088d3d
Patrick Carragher <carragher@live.se>
2020-09-08 17:31:58 +0200

    Proceed button exist test green

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -6,11 +6,11 @@
 
 			<div class="shipping">
 				<input type="radio" v-model="isClicked" id="set-adress-btn" :value="true">
-				<p class="changeAdress" @click="msg = 'You have successfully changed adress!'"></p> 
+				<p class="changeAdress" @click="msg = 'You have successfully changed adress!'">Change adress</p> 
 			</div>
 
 			<div class="payment">
-				<!-- <button class="proceed">proceed</button> -->
+				<button class="proceed">proceed</button>
 			</div>
 	</div>
 </template>
